4775c8d7abeaa263b737ae4a05b63ad1eb32b0c5
AnatoliyND <huykilik@gmail.com>
2022-12-11 20:03:14 +0300

    Handle ball to paddle collision

--- main.go
+++ main.go
@@ -25,6 +25,7 @@ var screen tcell.Screen
 var player1Paddle *GameObject
 var player2Paddle *GameObject
 var ball *GameObject
+var isGamePaused bool
 var debugLog string
 
 var gameObjects []*GameObject
@@ -45,17 +46,30 @@ func main() {
 }
 
 func UpdateState() {
+	if isGamePaused {
+		return
+	}
+
 	for i := range gameObjects {
 		gameObjects[i].row += gameObjects[i].velRow
 		gameObjects[i].col += gameObjects[i].velCol
 	}
 
+	debugLog = fmt.Sprintf("ball: row=%d, col=%d\npaddle 1: row=%d, col=%d\npaddle 2: row=%d, col=%d\n",
+		ball.row, ball.col, player1Paddle.row, player1Paddle.row, player2Paddle.col)
 	if CollidesWithWall(ball) {
 		ball.velRow = -ball.velRow
+	}
+
+	if CollidesWithPaddle(ball, player1Paddle) || CollidesWithPaddle(ball, player2Paddle) {
+		ball.velCol = -ball.velCol
 	}
 }
 
 func DrawState() {
+	if isGamePaused {
+		return
+	}
 	screen.Clear()
 	PrintString(0, 0, debugLog)
 	for _, obj := range gameObjects {
@@ -68,6 +82,18 @@ func DrawState() {
 func CollidesWithWall(obj *GameObject) bool {
 	_, screenHeight := screen.Size()
 	return obj.row+obj.velRow < 0 || obj.row+obj.velRow >= screenHeight
+}
+
+func CollidesWithPaddle(ball *GameObject, paddle *GameObject) bool {
+	var collidesOnColumn bool
+	if ball.col < paddle.col {
+		collidesOnColumn = ball.col+ball.velCol >= paddle.col
+	} else {
+		collidesOnColumn = ball.col+ball.velCol <= paddle.col
+	}
+	return collidesOnColumn &&
+		ball.row >= paddle.row &&
+		ball.row < paddle.row+paddle.height
 }
 
 func InitScreen() {
@@ -102,6 +128,8 @@ func HandlUserInput(key string) {
 		player2Paddle.row--
 	} else if key == "Down" && player2Paddle.row+player2Paddle.height < screenHeight {
 		player2Paddle.row++
+	} else if key == "Rune[p]" {
+		isGamePaused = !isGamePaused
 	}
 }
 
