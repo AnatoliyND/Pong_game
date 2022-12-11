27ed0dae156680e4e3908c6c41fb2eb0a8fc1c3c
AnatoliyND <huykilik@gmail.com>
2022-12-11 11:54:22 +0300

    Hello world

---
+++ go.mod
@@ -0,0 +1,13 @@
+module pong
+
+go 1.18
+
+require (
+	github.com/gdamore/encoding v1.0.0 // indirect
+	github.com/gdamore/tcell v1.4.0 // indirect
+	github.com/lucasb-eyer/go-colorful v1.0.3 // indirect
+	github.com/mattn/go-runewidth v0.0.14 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	golang.org/x/sys v0.0.0-20190626150813-e07cf5db2756 // indirect
+	golang.org/x/text v0.3.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/gdamore/tcell"
+)
+
+func emitStr(s tcell.Screen, x, y int, style tcell.Style, str string) {
+	for _, c := range str {
+		s.SetContent(x, y, c, nil, style)
+		x += 1
+	}
+}
+
+func displayHelloWorld(screen tcell.Screen) {
+	w, h := screen.Size()
+	screen.Clear()
+	style := tcell.StyleDefault.Foreground(tcell.ColorBlack).Background(tcell.ColorWhite)
+	emitStr(screen, w/2-7, h/2, style, "Hello, World!")
+	emitStr(screen, w/2-9, h/2+1, tcell.StyleDefault, "Press ESC to exit.")
+	screen.Show()
+}
+
+// This program just prints "Hello, World!".  Press ESC to exit.
+func main() {
+	screen, err := tcell.NewScreen()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		os.Exit(1)
+	}
+
+	if err := screen.Init(); err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		os.Exit(1)
+	}
+
+	defStyle := tcell.StyleDefault.
+		Background(tcell.ColorBlack).
+		Foreground(tcell.ColorWhite)
+	screen.SetStyle(defStyle)
+
+	displayHelloWorld(screen)
+
+	for {
+		switch ev := screen.PollEvent().(type) {
+		case *tcell.EventResize:
+			screen.Sync()
+			displayHelloWorld(screen)
+		case *tcell.EventKey:
+			if ev.Key() == tcell.KeyEscape {
+				screen.Fini()
+				os.Exit(0)
+			}
+		}
+	}
+}
